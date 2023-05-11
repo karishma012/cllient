25d0a647cbcb13a8bffdf2628e6ce4561b6102b2
karishma012 <115379019+karishma012@users.noreply.github.com>
2023-05-11 19:14:32 +0530

    Ecommerce website

---
+++ src/pages/Contact.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Layout from '../components/Layout/Layout'
+
+const Contact = () => {
+  return (
+
+      <Layout>
+        <h1>contact page</h1>
+      </Layout>
+    
+  )
+}
+
+export default Contact
---
+++ src/pages/Pagenotfound.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Layout from '../components/Layout/Layout'
+
+const Pagenotfound = () => {
+  return (
+
+      <Layout>
+        <h1>not found the page</h1>
+      </Layout>
+    
+  )
+}
+
+export default Pagenotfound
