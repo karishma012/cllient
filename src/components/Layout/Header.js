5843b38b0ca8095c3e8431ae5556ed76d44efff0
karishma012 <115379019+karishma012@users.noreply.github.com>
2023-05-20 18:55:24 +0530

    jhfkjfh

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,39 +1,42 @@
 import React from 'react'
-import {NavLink, Link} from 'react-router-dom'
+import { NavLink, Link } from 'react-router-dom'
+import { GiShop } from 'react-icons/gi'
 const Header = () => {
-  return (
-    <div>
-      <nav className="navbar navbar-expand-lg bg-body-tertiary">
-  <div className="container-fluid">
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
-      <Link to="/" className="navbar-brand" >ECOMMERCE APP</Link>
-      <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <NavLink to="/" className="nav-link " aria-current="page" >Home</NavLink>
-        </li>
-        <li className="nav-item">
-          <NavLink to="/category" className="nav-link " >CATEGORY</NavLink>
-        </li>
-        <li className="nav-item">
-          <NavLink to="/register" className="nav-link" >REGISTER</NavLink>
-        </li>
-        <li className="nav-item">
-          <NavLink to="/login" className="nav-link" >LOGIN</NavLink>
-        </li>
-        <li className="nav-item">
-          <NavLink to="/cart" className="nav-link" >CART(0)</NavLink>
-        </li>
-        
-      </ul>
-     
-    </div>
-  </div>
-</nav>
-    </div>
-  )
+    return (
+        <div>
+            <nav className="navbar navbar-expand-lg bg-body-tertiary">
+                <div className="container-fluid">
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+                        <Link to="/" className="navbar-brand" >
+                            <GiShop /> ECOMMERCE APP</Link>
+
+                        <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <NavLink to="/" className="nav-link " aria-current="page" >Home</NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink to="/category" className="nav-link " >CATEGORY</NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink to="/register" className="nav-link" >REGISTER</NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink to="/login" className="nav-link" >LOGIN</NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink to="/cart" className="nav-link" >CART(0)</NavLink>
+                            </li>
+
+                        </ul>
+
+                    </div>
+                </div>
+            </nav>
+        </div>
+    )
 }
 
 export default Header
--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,18 +1,34 @@
-import React from 'react'
-import Header from './Header'
-import Footer from './Footer'
-//rafce
-const Layout = ({children}) => {
+import React from "react";
+import Footer from "./Footer";
+import Header from "./Header";
+import { Helmet } from "react-helmet";
+import { Toaster } from "react-hot-toast";
+const Layout = ({ children, title, description, keywords, author }) => {
   return (
     <div>
-      <Header/>
-      <main style ={{minHeight : '80vh'}}>
-        
-      {children}
-      </main>
-     <Footer/>
-    </div>
-  )
-}
+      <Helmet>
+        <meta charSet="utf-8" />
+        <meta name="description" content={description} />
+        <meta name="keywords" content={keywords} />
+        <meta name="author" content={author} />
+        <title>{title}</title>
+      </Helmet>
+      <Header />
+      <main style={{ minHeight: "70vh" }}>
+        <Toaster />
 
-export default Layout
+        {children}
+      </main>
+      <Footer />
+    </div>
+  );
+};
+
+Layout.defaultProps = {
+  title: "Ecommerce app - shop now",
+  description: "mern stack project",
+  keywords: "mern,react,node,mongodb",
+  author: "Techinfoyt",
+};
+
+export default Layout;
